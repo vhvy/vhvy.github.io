768bff33ce3b29ba48df11de23a678b807c2c5bc
vhvy <vhvy@qq.com>
2021-06-06 23:08:10 +0800

    GitHub Actions Auto Builder at 2021-06-06 23:08:09

---
+++ js/content.js
@@ -0,0 +1,37 @@
+class LazyLoadImg {
+    constructor() {
+        this.options = {
+            threshold: .25
+        };
+        this.observer = new IntersectionObserver(this.handle.bind(this), this.options);
+        this.init();
+    }
+
+    handle(entries) {
+        entries.forEach(({ isIntersecting, target }) => {
+            isIntersecting && this.loadImg(target);
+        })
+    }
+
+    loadImg(el) {
+        this.observer.unobserve(el);
+        const src = el.getAttribute('data-original');
+        let image = new Image();
+        image.onload = () => {
+            el.removeAttribute('data-original');
+            el.setAttribute('src', src);
+            image = null;
+        }
+
+        image.src = src;
+    }
+
+    init() {
+        Array.from(document.querySelectorAll('img[data-original]'))
+            .map(el => {
+                this.observer.observe(el);
+            });
+    }
+}
+
+new LazyLoadImg;
